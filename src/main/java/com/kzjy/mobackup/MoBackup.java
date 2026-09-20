4a02bc437e28538b8d13b720acff1048bf627d89
TerrySet-NA <TerrySet97@gmail.com>
2026-09-20 17:55:34 +0800

    Add files via upload

--- src/main/java/com/kzjy/mobackup/MoBackup.java
+++ src/main/java/com/kzjy/mobackup/MoBackup.java
@@ -1,3 +1,11 @@
+/*
+ * Copyright (C) 2026 TerrySet
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ */
+
 package com.kzjy.mobackup;
 
 import java.util.List;
